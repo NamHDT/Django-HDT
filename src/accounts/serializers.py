b597cbeae42df4cfdd7e72567d9949b3f637c3ee
NamHDT <namnh.hdt@gmail.com>
2022-02-26 15:34:20 +0700

    tesst
    Teest

--- src/accounts/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Accounts
-from django.contrib.auth.models import User
-
-class AccountsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Accounts
-        fields = '__all__'
